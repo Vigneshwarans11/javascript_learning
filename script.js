9ed81fbff7c30a595ae0f2c7d46b2581eea27e7f
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2026-02-03 07:59:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let amount = 1000;
+        console.log("your bill amount is RS."+ amount);
+
+        const pi = 3.14
+        // pi=67
+        console.log(pi)
+
+        //user input
+        // let name= prompt("whats is your name")
+        // console.log("hi"+name)
+
+
+        // let tickets=prompt("how many tickets you want")
+        // console.log("you sholud pay",Number(tickets)*20)
+        
+        let a=prompt("how many")
+        console.log(Number(a)+4)
+
+
